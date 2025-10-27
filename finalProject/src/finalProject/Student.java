d31aa5e83cd27f702754653dddc8e0da82631ff8
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-10-27 14:55:29 -0400

    Added Student Class

---
+++ finalProject/src/finalProject/Student.java
@@ -0,0 +1,20 @@
+public class Student extends Person implements CourseInterface {
+    private Schedule schedule = new Schedule();
+
+    @Override
+    public void addCourse(Course course) {
+        schedule.addCourse(course);
+    }
+
+    @Override
+    public void removeCourse(Course course) {
+        schedule.removeCourse(course);
+    }
+
+    public void viewSchedule() {
+        System.out.println("Schedule for " + name + ":");
+        for (Course c : schedule.getCourses()) {
+            System.out.println(c);
+        }
+    }
+}
