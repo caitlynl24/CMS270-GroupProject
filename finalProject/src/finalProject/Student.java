a5aa58625dc9a8c53d93977bdf67b5b21bfaf45f
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-11-06 20:55:48 -0500

    Updated Student.java

--- finalProject/src/finalProject/Student.java
+++ finalProject/src/finalProject/Student.java
@@ -5,12 +5,24 @@ public class Student extends Person implements CourseInterface {
 
     @Override
     public void addCourse(Course course) {
-        schedule.addCourse(course);
+        if(course.isFull()) {
+            System.out.println("Cannot add course. It is full.");
+            return;
+        }
+        if(!schedule.hasConflict(course)) {
+            schedule.addCourse(course);
+            course.enrollStudent(this.id);
+            System.out.println("Course added successfully.");
+        } else {
+            System.out.println("Schedule conflict detected.");
+        }
     }
 
     @Override
     public void removeCourse(Course course) {
         schedule.removeCourse(course);
+        course.removeStudent(this.id);
+        System.out.println("Course removed.");
     }
 
     public void viewSchedule() {
