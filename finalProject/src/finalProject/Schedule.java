64f57e73b1cce02d1940d0dc1f891e10869baa38
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-11-07 09:19:44 -0500

    Updated Schedule.java

--- finalProject/src/finalProject/Schedule.java
+++ finalProject/src/finalProject/Schedule.java
@@ -19,11 +19,17 @@ public class Schedule {
 
     public boolean hasConflict(Course course) {
         for (Course c : courses) {
-            if (c.getDay().equals(course.getDay()) && c.getTime().equals(course.getTime())) {
-                return true;
+            if (c.getDay().equals(course.getDay())) {
+                if (timeOverlap(c.getStartTime(), c.getEndTime(), course.getStartTime(), course.getEndTime())) {
+                    return true;
+                }
             }
         }
         return false;
+    }
+
+    private boolean timeOverlap(String start1, String end1, String start2, String end2) {
+        return start1.compareTo(end2) < 0 && start2.compareTo(end1) < 0;
     }
 
     public ArrayList<Course> getCourses() {
